b8bdd595b55e121d34dd97fcc65a689e30062b44
ColinKammer <colin.kammer@gmx.de>
2024-01-19 22:44:01 +0100

    fix linux build

--- hookImpl/cjkAssert_cassert.cpp
+++ hookImpl/cjkAssert_cassert.cpp
@@ -1,8 +1,7 @@
-#pragma once
 #include <iostream>
 #include <cassert>
 
-#include "CjkAssert.h"
+#include "cjkAssert.h"
 
 namespace cjk
 {
--- hookImpl/jsonString_nlohmann.cpp
+++ hookImpl/jsonString_nlohmann.cpp
@@ -1,14 +1,14 @@
-#pragma once
-#include "JsonString.h"
-#include "CjkAssert.h"
+#include "cjkAssert.h"
+#include "jsonString.h"
+
 
 #include "nlohmann/json.hpp"
 
 namespace cjk {
 
-	JsonString::JsonString(const std::string& s)
-		:m_value(s)
-	{
-		CJK_ASSERT_NOTHROW(nlohmann::json::parse(m_value), "JsonString is not actually json parsable");
-	}
+JsonString::JsonString(const std::string& s)
+    : m_value(s)
+{
+    CJK_ASSERT_NOTHROW(nlohmann::json::parse(m_value), "JsonString is not actually json parsable");
+}
 }
